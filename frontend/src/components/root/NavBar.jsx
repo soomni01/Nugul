90fcc931a48b499d873579c9d0f3868d851cacc5
soomni01 <dsgh8933@naver.com>
2024-11-26 16:44:05 +0900

    Design: navbar 크기 조절

--- frontend/src/components/root/NavBar.jsx
+++ frontend/src/components/root/NavBar.jsx
@@ -1,16 +1,40 @@
 import { useNavigate } from "react-router-dom";
 import { Box, Flex } from "@chakra-ui/react";
 
+function NavbarItem({ children, ...rest }) {
+  return (
+    <Box
+      css={{
+        paddingX: "20px",
+        paddingY: "15px",
+      }}
+      _hover={{
+        bgColor: "blue.300",
+        cursor: "pointer",
+      }}
+      {...rest}
+    >
+      {children}
+    </Box>
+  );
+}
+
 export function Navbar() {
   const navigate = useNavigate();
   return (
     <Flex gap={3}>
-      <Box onClick={() => navigate("/")}>HOME</Box>
-      <Box onClick={() => navigate("/product/list")}>중고거래</Box>
-      <Box onClick={() => navigate("/sh")}>나눔</Box>
-      <Box onClick={() => navigate("/chat")}>채팅</Box>
-      <Box onClick={() => navigate("/add")}>문의하기</Box>
-      <Box onClick={() => navigate(`/member/${id}`)}>마이페이지</Box>
+      <NavbarItem onClick={() => navigate("/")}>홈</NavbarItem>
+      <NavbarItem onClick={() => navigate("/product/list")}>
+        중고거래
+      </NavbarItem>
+      <NavbarItem onClick={() => navigate("/product/share/list")}>
+        나눔
+      </NavbarItem>
+      <NavbarItem onClick={() => navigate("/chat")}>채팅</NavbarItem>
+      <NavbarItem onClick={() => navigate("/inquiry")}>문의하기</NavbarItem>
+      <NavbarItem onClick={() => navigate(`/member/${id}`)}>
+        마이페이지
+      </NavbarItem>
     </Flex>
   );
 }
