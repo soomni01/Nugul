6f9cde864eb8c5deaa5dbd678f6d3f573871f71e
soomni01 <dsgh8933@naver.com>
2024-11-26 14:46:13 +0900

    be : spring web 라이브러리 추가 jwt 설정 추가
    fe : axios 인터셉터 return 코드 추가

--- backend/src/main/java/com/example/backend/config/AppConfiguration.java
+++ backend/src/main/java/com/example/backend/config/AppConfiguration.java
@@ -1,15 +1,35 @@
 package com.example.backend.config;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 @EnableMethodSecurity
 public class AppConfiguration {
+
+    @Value("classpath:secret/app.pub")
+    RSAPublicKey pub;
+
+    @Value("classpath:secret/app.key")
+    RSAPrivateKey priv;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -18,6 +38,19 @@ public class AppConfiguration {
 
         
         return http.build();
+    }
+
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(pub).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(pub).privateKey(priv).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
     }
 
 }
