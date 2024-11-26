b5ed4a38312be19d7ff80f739f6c27a237b8d63f
keaimk <fly1043@naver.com>
2024-11-26 18:08:22 +0900

    style : 글자 정리

--- frontend/src/components/root/NavBar.jsx
+++ frontend/src/components/root/NavBar.jsx
@@ -6,10 +6,10 @@ export function Navbar() {
   return (
     <Flex gap={3}>
       <Box onClick={() => navigate("/")}>HOME</Box>
-      <Box onClick={() => navigate("/product/list")}>중고거래</Box>
-      <Box onClick={() => navigate("/sh")}>나눔</Box>
+      <Box onClick={() => navigate("/product/list")}>중고 거래</Box>
+      <Box onClick={() => navigate("/sh")}>무료 나눔</Box>
       <Box onClick={() => navigate("/chat")}>채팅</Box>
-      <Box onClick={() => navigate("/add")}>문의하기</Box>
+      <Box onClick={() => navigate("/add")}>문의</Box>
       <Box onClick={() => navigate(`/member/${id}`)}>마이페이지</Box>
     </Flex>
   );
