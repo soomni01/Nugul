c77c93637009665023fc1f367db6cdb71795bfdb
qortn4925 <qortn4925@naver.com>
2024-11-26 16:40:01 +0900

    Test:
    백엔드 에서 websoket 받기 위해
    추가한 파일들

---
+++ backend/src/main/java/com/example/backend/config/WebSocketConfig.java
@@ -0,0 +1,8 @@
+package com.example.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebSocketMe
+public class WebSocketConfig implements WebSocketConfigurer {
+}
---
+++ backend/src/main/java/com/example/backend/controller/chat.java
@@ -0,0 +1,14 @@
+package com.example.backend.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/chat")
+@RequiredArgsConstructor  // 이거 < 필요한 생성자 만들엊 ㅜ는거
+public class chat {
+
+    
+}
