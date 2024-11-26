d56964bde7fd309cd1ae9a9b4272b453f1a53adc
qortn4925 <qortn4925@naver.com>
2024-11-26 16:39:18 +0900

    Style:
    heading 으로 감싸줌

--- frontend/src/page/chat/ChatList.jsx
+++ frontend/src/page/chat/ChatList.jsx
@@ -1,6 +1,14 @@
 import { Box, Heading } from "@chakra-ui/react";
 
 export function ChatList() {
+  // WebSocket 객체 생성 ( 서버의 Websocket) 포인트를 사용
+  const socket = new WebSocket("wss://localhost:8080/chat");
+
+  socket.onopen = function (evnet) {
+    console.log("webSocket is connect");
+    socket.send(`hello , Server`);
+  };
+
   return (
     <Box my={5}>
       <Heading>채팅 리스트 화면</Heading>
