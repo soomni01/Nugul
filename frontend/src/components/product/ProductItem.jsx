9b1663c3d6f4fe4f94b151a2e903f25f3b4af6ca
soomni01 <dsgh8933@naver.com>
2024-12-02 11:13:10 +0900

    design: productItem의 좋아요 버튼 등 ui 수정

--- frontend/src/components/product/ProductItem.jsx
+++ frontend/src/components/product/ProductItem.jsx
@@ -1,4 +1,13 @@
-import { Badge, Box, Card, HStack, Image } from "@chakra-ui/react";
+import {
+  Badge,
+  Box,
+  Card,
+  Flex,
+  Heading,
+  HStack,
+  Image,
+  VStack,
+} from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../ui/button.jsx";
 import { GoHeart } from "react-icons/go";
@@ -6,6 +15,7 @@ import { categories } from "../category/CategoryContainer.jsx";
 import { PiCurrencyKrwBold } from "react-icons/pi";
 import { getDaysAgo } from "../product/ProductDate.jsx";
 import React from "react";
+import axios from "axios";
 
 export function ProductItem({ product }) {
   const navigate = useNavigate();
@@ -17,15 +27,28 @@ export function ProductItem({ product }) {
   // 날짜 차이를 계산하는 함수
   const daysAgo = getDaysAgo(product.createdAt);
 
+  const handleLikeClick = () => {
+    axios
+      .post("/api/board/like", {
+        id: product.productId,
+      })
+      .then()
+      .catch()
+      .finally();
+  };
   return (
     <Card.Root maxW="sm" overflow="hidden">
       <Image src="/image/productItem.png" alt={product.productName} />
-      <Card.Body gap="2">
+      <Card.Body gap="2" p="4">
         <HStack justify="space-between" w="100%">
           <Badge>{categoryLabel}</Badge>
           <Box>{daysAgo}</Box>
         </HStack>
-        <Card.Title>{product.productName}</Card.Title>
+        <Card.Title>
+          <Box isTruncated maxWidth="100%">
+            {product.productName}
+          </Box>
+        </Card.Title>
         <Card.Description>
           <HStack gap="1">
             <PiCurrencyKrwBold />
@@ -33,16 +56,24 @@ export function ProductItem({ product }) {
           </HStack>
         </Card.Description>
       </Card.Body>
-      <Card.Footer gap="2">
-        <Button
-          onClick={() => navigate(`/product/view/${product.productId}`)}
-          variant="solid"
-        >
-          상품보기
-        </Button>
-        <Box>
-          <GoHeart />
-        </Box>
+      <Card.Footer justify="space-between" px={3} pb={1}>
+        <Flex justify="space-between" align="center" w="100%">
+          <Button
+            w="70%"
+            onClick={() => navigate(`/product/view/${product.productId}`)}
+            variant="solid"
+          >
+            상품보기
+          </Button>
+          <VStack gap={1} mt={3}>
+            <Box onClick={handleLikeClick} cursor="pointer">
+              <Heading fontSize="3xl">
+                <GoHeart />
+              </Heading>
+            </Box>
+            <Box>3</Box>
+          </VStack>
+        </Flex>
       </Card.Footer>
     </Card.Root>
   );
