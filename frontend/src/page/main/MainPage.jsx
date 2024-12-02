e655dfa393d8bac1414b1cd2e14621393f10e393
soomni01 <dsgh8933@naver.com>
2024-12-02 17:50:40 +0900

    feat: swiper 수정중

--- frontend/src/page/main/MainPage.jsx
+++ frontend/src/page/main/MainPage.jsx
@@ -1,6 +1,12 @@
 import { Box, Heading, Image, Separator } from "@chakra-ui/react";
 import { CategoryContainer } from "../../components/category/CategoryContainer.jsx";
 import { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination } from "swiper/modules";
+import "./MainPage.css";
 
 export function MainPage() {
   const [selectedCategory, setSelectedCategory] = useState("all");
@@ -19,6 +25,25 @@ export function MainPage() {
         selectedCategory={selectedCategory}
         onCategorySelect={handleCategorySelect}
       />
+      {/* swiper : infinite loop */}
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+      </Swiper>
+
       <Separator my={10} />
       <Heading>중고 아이템</Heading>
       <Box w="100%" h="300px" background="beige" my="3"></Box>
