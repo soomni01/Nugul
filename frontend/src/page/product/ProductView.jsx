587c356327af54898f1a1a2dde61f1366735c3ab
soomni01 <dsgh8933@naver.com>
2024-12-02 10:23:31 +0900

    refactor: 상세페이지 지도에 고정된 마커 수정

--- frontend/src/page/product/ProductView.jsx
+++ frontend/src/page/product/ProductView.jsx
@@ -120,7 +120,12 @@ export function ProductView() {
         <Box>
           <Map
             className="map"
-            center={markerPosition || { lat: 33.450701, lng: 126.570667 }}
+            center={
+              markerPosition || {
+                lat: product.latitude,
+                lng: product.longitude,
+              }
+            }
             level={3}
             style={{ width: "100%", height: "400px" }}
           >
