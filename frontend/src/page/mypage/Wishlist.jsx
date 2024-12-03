462373fe795e935560a588175a3d951a6a493da9
soomni01 <dsgh8933@naver.com>
2024-12-03 16:24:21 +0900

    feat: 새로고침해도 보고있던 마이페이지 보기

--- frontend/src/page/mypage/Wishlist.jsx
+++ frontend/src/page/mypage/Wishlist.jsx
@@ -1,12 +1,66 @@
-import { Box, Heading } from "@chakra-ui/react";
+import {
+  Badge,
+  Box,
+  Card,
+  Heading,
+  HStack,
+  Image,
+  Spinner,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Button } from "../../components/ui/button.jsx";
 
 export function Wishlist() {
+  const [productList, setProductList] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+
+    axios
+      .get("/api/myPage/like")
+      .then((res) => res.data)
+      .then((data) => {
+        setProductList(data.list);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log("관심 상품 정보를 가져오는 데 실패했습니다.", error);
+      });
+  }, []);
+  console.log(productList);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
   return (
     <Box>
       <Heading size="lg" mb={4}>
-        찜 목록
+        관심 목록
       </Heading>
-      <Box>여기에는 찜 목록이 표시됩니다.</Box>
+      <Box>
+        <Card.Root flexDirection="row" maxH="150px">
+          <Image maxW="150px" objectFit="cover" src="/image/productItem.png" />
+          <Box>
+            <Card.Body>
+              <Card.Title mb="2">The perfect latte</Card.Title>
+              <Card.Description>
+                Caffè latte is a coffee beverage of Italian origin made with
+                espresso and steamed milk.
+              </Card.Description>
+              <HStack mt="4">
+                <Badge>Hot</Badge>
+                <Badge>Caffeine</Badge>
+              </HStack>
+            </Card.Body>
+            <Card.Footer>
+              <Button>Buy Latte</Button>
+            </Card.Footer>
+          </Box>
+        </Card.Root>
+      </Box>
     </Box>
   );
 }
