8fda8faee327866de775f7dec95caef104be24c9
seokminlee24 <lsm67286564@gmail.com>
2024-12-06 12:48:57 +0900

    feat: 댓글이 리스트 추가 댓글이 보임 다만 추가할떄 새로고침 및 아이디아 닉네이임 아님

--- backend/src/main/java/com/example/backend/controller/comment/CommentController.java
+++ backend/src/main/java/com/example/backend/controller/comment/CommentController.java
@@ -5,16 +5,20 @@ import com.example.backend.service.comment.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/comment")
 public class CommentController {
     final CommentService service;
+
+    @GetMapping("commentList/{boardId}")
+    public List<Comment> commentList(@PathVariable Integer boardId) {
+        return service.commentList(boardId);
+    }
 
     @PostMapping("commentAdd")
     @PreAuthorize("isAuthenticated()")
--- backend/src/main/java/com/example/backend/service/comment/CommentService.java
+++ backend/src/main/java/com/example/backend/service/comment/CommentService.java
@@ -7,6 +7,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -16,5 +18,9 @@ public class CommentService {
     public void commentAdd(Comment comment, Authentication authentication) {
         comment.setMemberId(authentication.getName());
         mapper.insert(comment);
+    }
+
+    public List<Comment> commentList(Integer boardId) {
+        return mapper.selectByBoardId(boardId);
     }
 }
