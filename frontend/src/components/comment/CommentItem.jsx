4ab672a1bcf5677ba6f5ad9cb4ea4c08bffe9b63
seokminlee24 <lsm67286564@gmail.com>
2024-12-07 23:19:09 +0900

    feat: 댓글 삭제 dialog

--- frontend/src/components/comment/CommentItem.jsx
+++ frontend/src/components/comment/CommentItem.jsx
@@ -1,7 +1,50 @@
-import {Box, Flex, HStack} from "@chakra-ui/react";
+import {Box, Dialog, Flex, HStack} from "@chakra-ui/react";
 import {Button} from "../ui/button.jsx";
+import {useContext, useState} from "react";
+import {
+    DialogActionTrigger,
+    DialogBody,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogRoot,
+    DialogTitle,
+    DialogTrigger
+} from "../ui/dialog.jsx";
+import {AuthenticationContext} from "../context/AuthenticationProvider.jsx";
+
+function DeleteButton({ onClick }) {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <>
+            <DialogRoot open={open} onOpenChange={(e) => setOpen(e.open)}>
+                <DialogTrigger asChild>
+                    <Button colorPalette={"red"}>삭제</Button>
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>삭제 확인</DialogTitle>
+                    </DialogHeader>
+                    <DialogBody>
+                        <p>댓글을 삭제하시겠습니까?</p>
+                    </DialogBody>
+                    <DialogFooter>
+                        <DialogActionTrigger>
+                            <Button variant={"outline"}>취소</Button>
+                        </DialogActionTrigger>
+                        <Button colorPalette={"red"} onClick={onClick}>
+                            삭제
+                        </Button>
+                    </DialogFooter>
+                </DialogContent>
+            </DialogRoot>
+        </>
+    );
+}
 
 export function CommentItem({ comment,onDeleteClick }) {
+    const {hasAccess} = useContext(AuthenticationContext)
   return (
     <HStack boarder={"1px solid black"} m={5}>
         <Box flex={1}>
@@ -11,12 +54,13 @@ export function CommentItem({ comment,onDeleteClick }) {
       </Flex>
       <p>{comment.comment}</p>
         </Box>
-        <Box>
-            <Button colorPalette={"purple"}>수정</Button>
-            <Button colorPalette={"red"} onClick={() => onDeleteClick(comment.commentId)}>
-                삭제
-            </Button>
-        </Box>
+        {hasAccess(comment.memberId) && (
+            <Box>
+                <Button colorPalette={"purple"}>수정</Button>
+
+                <DeleteButton onClick={() => onDeleteClick(comment.commentId)} />
+            </Box>
+        )}
     </HStack>
   );
 }
