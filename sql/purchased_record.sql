f07beefc93ba077116c604a6311026b0f34d2811
keaimk <fly1043@naver.com>
2024-12-09 16:01:27 +0900

    test : 임시 데이터 삽입

--- sql/purchased_record.sql
+++ sql/purchased_record.sql
@@ -14,3 +14,24 @@ ALTER TABLE purchased_record
 
 INSERT INTO purchased_record (buyer_id, product_id)
 VALUES ('sm@naver.com', 55);
+
+DESC purchased_record;
+
+SELECT *
+FROM purchased_record;
+
+INSERT INTO purchased_record (buyer_id, product_id, date, seller_id, product_name, location_name, review_status, price)
+VALUES ('coogie@naver.com', 130, '2024-12-09', 'sy@naver.com', '상품1', 'Seoul', 'uncompleted', 1111);
+
+INSERT INTO purchased_record (buyer_id, product_id, date, seller_id, product_name, location_name, review_status, price)
+VALUES ('coogie@naver.com', 131, '2024-12-09', '@naver.com', '상품2', 'Seoul', 'uncompleted', 2222);
+
+SELECT DATE_FORMAT(created_at, '%Y-%m')                                 AS month,
+       SUM(CASE WHEN writer = 'coogie@naver.com' THEN price ELSE 0 END) AS total_sales
+FROM product
+WHERE status = 'For Sale';
+
+
+SELECT DATE_FORMAT(date, '%Y-%m')                                         AS month,
+       SUM(CASE WHEN buyer_id = 'coogie@naver.com' THEN price ELSE 0 END) AS total_purchases
+FROM purchased_record;
