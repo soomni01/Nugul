75dcd95d7bf841470e5fddc681089bbd81fc7cc9
qortn4925 <qortn4925@naver.com>
2024-12-09 15:23:21 +0900

    feat:  displayPlaces 까지

--- frontend/src/page/map/ViewMap.jsx
+++ frontend/src/page/map/ViewMap.jsx
@@ -20,17 +20,32 @@ function ViewMap() {
   const [customOverlay, setCustomOverlay] = useState(null);
   const [overlayMarkers, setOverlayMarkers] = useState([]);
   const [currCategory, setCurrCategory] = useState("");
-
+  var contentNode = document.createElement("div");
   useEffect(() => {
-    addCategoryClickEvent();
-
     // 아무것도 안할때의 이벤트 추가해놓기
-    // map.addListener(map, "idle", searchPlaces);
+    var ps = new kakao.maps.services.Places(map);
+    // map.event.addListener(map, "idle", searchCategoryPlaces);
+    contentNode.className = "placeinfo_wrap";
+    // 커스텀 오버레이의 컨텐츠 노드에 mousedown, touchstart 이벤트가 발생했을때
+    // 지도 객체에 이벤트가 전달되지 않도록 이벤트 핸들러로 kakao.maps.event.preventMap 메소드를 등록합니다
+    addEventHandle(contentNode, "mousedown", kakao.maps.event.preventMap);
+    addEventHandle(contentNode, "touchstart", kakao.maps.event.preventMap);
+    // customOverlay.setContent(contentNode);
+    // 카테고리 이벤트 추가
+    addCategoryClickEvent();
   }, []);
+
+  function addEventHandle(target, type, callback) {
+    if (target.addEventListener) {
+      target.addEventListener(type, callback);
+    } else {
+      target.attachEvent("on" + type, callback);
+    }
+  }
 
   const handleSearch = () => {
     if (!map) return;
-    const ps = new kakao.maps.services.Places();
+    const ps = new kakao.maps.services.Places(map);
 
     ps.keywordSearch(locationName, (data, status, _pagination) => {
       if (status === kakao.maps.services.Status.OK) {
@@ -177,7 +192,73 @@ function ViewMap() {
     } else {
       setCurrCategory(id);
       changeCategoryClass(this);
+      //  이거 흠
       searchPlaces();
+    }
+  }
+
+  // 클릭된 카테고리에만 클릭된 스타일을 적용하는 함수입니다
+  function changeCategoryClass(el) {
+    var category = document.getElementById("category"),
+      children = category.children,
+      i;
+
+    for (i = 0; i < children.length; i++) {
+      children[i].className = "";
+    }
+
+    if (el) {
+      el.className = "on";
+    }
+  }
+
+  // 카테고리 검색을 요청하는 함수입니다
+  function searchCategoryPlaces() {
+    if (!currCategory) {
+      return;
+    }
+
+    // 커스텀 오버레이를 숨깁니다
+    customOverlay.setMap(null);
+
+    // 지도에 표시되고 있는 마커를 제거합니다
+    // removeMarker();
+
+    ps.categorySearch(currCategory, placesSearchCB, { useMapBounds: true });
+  }
+
+  function placesSearchCB(data, status, pagination) {
+    if (status === kakao.maps.services.Status.OK) {
+      // 정상적으로 검색이 완료됐으면 지도에 마커를 표출합니다
+      displayPlaces(data);
+    } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
+      // 검색결과가 없는경우 해야할 처리가 있다면 이곳에 작성해 주세요
+    } else if (status === kakao.maps.services.Status.ERROR) {
+      // 에러로 인해 검색결과가 나오지 않은 경우 해야할 처리가 있다면 이곳에 작성해 주세요
+    }
+  }
+
+  function displayPlaces(places) {
+    // 몇번째 카테고리가 선택되어 있는지 얻어옵니다
+    // 이 순서는 스프라이트 이미지에서의 위치를 계산하는데 사용됩니다
+    var order = document
+      .getElementById(currCategory)
+      .getAttribute("data-order");
+
+    for (var i = 0; i < places.length; i++) {
+      // 마커를 생성하고 지도에 표시합니다
+      var marker = addMarker(
+        new kakao.maps.LatLng(places[i].y, places[i].x),
+        order,
+      );
+
+      // 마커와 검색결과 항목을 클릭 했을 때
+      // 장소정보를 표출하도록 클릭 이벤트를 등록합니다
+      (function (marker, place) {
+        kakao.maps.event.addListener(marker, "click", function () {
+          displayPlaceInfo(place);
+        });
+      })(marker, places[i]);
     }
   }
 
