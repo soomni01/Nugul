1897322dbaca8d7e3282c838e64a386b49b008bc
seokminlee24 <lsm67286564@gmail.com>
2024-12-11 14:18:47 +0900

    feat: 게시판 카테고리 변경

--- frontend/src/components/board/BoardCategories.jsx
+++ frontend/src/components/board/BoardCategories.jsx
@@ -1,9 +1,9 @@
 // boardCategories 배열을 정의하고 export
 export const BoardCategories = [
     {label: "전체", value: "all"},
-    {label: "의류", value: "clothes"},
-    {label: "잡화", value: "miscellaneous"},
-    {label: "식품", value: "food"},
-    {label: "뷰티", value: "beauty"},
-    {label: "디지털 기기", value: "digital"},
+    {label: "맛집", value: "restaurant"},
+    {label: "생활", value: "life"},
+    {label: "리뷰", value: "review"},
+    {label: "고민", value: "concern"},
+    {label: "기타", value: "other"},
 ];
