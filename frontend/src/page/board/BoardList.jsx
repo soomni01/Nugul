d9810464b33b4d74c98af34215c106822bb7189e
seokminlee24 <lsm67286564@gmail.com>
2024-12-16 09:57:58 +0900

    feat 페이징: 카테고리 게시물 없을떄 페이징 안보이게

--- frontend/src/page/board/BoardList.jsx
+++ frontend/src/page/board/BoardList.jsx
@@ -151,53 +151,75 @@ export function BoardList() {
         )}
       </Flex>
 
+      {/* 게시물 리스트 */}
       {boardList.length > 0 ? (
-        <Table.Root interactive>
-          <Table.Header>
-            <Table.Row>
-              <Table.ColumnHeader>번호</Table.ColumnHeader>
-              <Table.ColumnHeader>제목</Table.ColumnHeader>
-              <Table.ColumnHeader>작성자</Table.ColumnHeader>
-              <Table.ColumnHeader>카테고리</Table.ColumnHeader>
-              <Table.ColumnHeader>작성날짜</Table.ColumnHeader>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {boardList.map((board) => (
-              <Table.Row
-                onClick={() => handleRowClick(board.boardId)}
-                key={board.boardId}
-              >
-                <Table.Cell>{board.boardId}</Table.Cell>
-                <Table.Cell>
-                  {board.title}
-                  {board.countComment > 0 && (
-                    <Badge variant={"subtle"} colorPalette={"green"}>
-                      <FaCommentDots />
-                      {board.countComment}
-                    </Badge>
-                  )}
-                  {board.countFile > 0 && (
-                    <Badge variant={"subtle"} colorPalette={"gray"}>
-                      <FaImages />
-                      {board.countFile}
-                    </Badge>
-                  )}
-                </Table.Cell>
-                <Table.Cell>{board.writer}</Table.Cell>
-                <Table.Cell>
-                  {BoardCategories.find((cat) => cat.value === board.category)
-                    ?.label || board.category}
-                </Table.Cell>
-                <Table.Cell>{board.createdAt}</Table.Cell>
+        <>
+          <Table.Root interactive>
+            <Table.Header>
+              <Table.Row>
+                <Table.ColumnHeader>번호</Table.ColumnHeader>
+                <Table.ColumnHeader>제목</Table.ColumnHeader>
+                <Table.ColumnHeader>작성자</Table.ColumnHeader>
+                <Table.ColumnHeader>카테고리</Table.ColumnHeader>
+                <Table.ColumnHeader>작성날짜</Table.ColumnHeader>
               </Table.Row>
-            ))}
-          </Table.Body>
-        </Table.Root>
+            </Table.Header>
+            <Table.Body>
+              {boardList.map((board) => (
+                <Table.Row
+                  onClick={() => handleRowClick(board.boardId)}
+                  key={board.boardId}
+                >
+                  <Table.Cell>{board.boardId}</Table.Cell>
+                  <Table.Cell>
+                    {board.title}
+                    {board.countComment > 0 && (
+                      <Badge variant={"subtle"} colorPalette={"green"}>
+                        <FaCommentDots />
+                        {board.countComment}
+                      </Badge>
+                    )}
+                    {board.countFile > 0 && (
+                      <Badge variant={"subtle"} colorPalette={"gray"}>
+                        <FaImages />
+                        {board.countFile}
+                      </Badge>
+                    )}
+                  </Table.Cell>
+                  <Table.Cell>{board.writer}</Table.Cell>
+                  <Table.Cell>
+                    {BoardCategories.find((cat) => cat.value === board.category)
+                      ?.label || board.category}
+                  </Table.Cell>
+                  <Table.Cell>{board.createdAt}</Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table.Root>
+
+          {/* 페이징 컴포넌트 */}
+          {count > 0 && (
+            <PaginationRoot
+              onPageChange={handlePageChange}
+              count={count}
+              pageSize={10}
+              page={page}
+            >
+              <Flex justifyContent="center" mt={4}>
+                <HStack>
+                  <PaginationPrevTrigger />
+                  <PaginationItems />
+                  <PaginationNextTrigger />
+                </HStack>
+              </Flex>
+            </PaginationRoot>
+          )}
+        </>
       ) : (
         <p>조회된 결과가 없습니다.</p>
       )}
 
+      {/* 검색 UI */}
       <HStack mt={4}>
         <Box>
           <select
@@ -216,21 +238,6 @@ export function BoardList() {
         />
         <Button onClick={handleSearchClick}>검색</Button>
       </HStack>
-
-      <PaginationRoot
-        onPageChange={handlePageChange}
-        count={count}
-        pageSize={10}
-        page={page}
-      >
-        <Flex justifyContent="center" mt={4}>
-          <HStack>
-            <PaginationPrevTrigger />
-            <PaginationItems />
-            <PaginationNextTrigger />
-          </HStack>
-        </Flex>
-      </PaginationRoot>
     </Box>
   );
 }
