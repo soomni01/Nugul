df606e7d6c44d202344bbdee34247a8f9ed1e8de
soomni01 <dsgh8933@naver.com>
2024-12-16 15:12:08 +0900

    design: 상품 상세 페이지 ui 수정

--- frontend/src/page/product/ProductView.jsx
+++ frontend/src/page/product/ProductView.jsx
@@ -1,8 +1,16 @@
-import { Box, Flex, Input, Spinner, Stack, Textarea } from "@chakra-ui/react";
-import { Field } from "../../components/ui/field.jsx";
+import {
+  Badge,
+  Box,
+  Heading,
+  HStack,
+  Image,
+  Spacer,
+  Spinner,
+  Stack,
+  Text,
+  Textarea,
+} from "@chakra-ui/react";
 import { Button } from "../../components/ui/button.jsx";
-import { InputGroup } from "../../components/ui/input-group.jsx";
-import { PiCurrencyKrwBold } from "react-icons/pi";
 import React, { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
@@ -20,9 +28,13 @@ import { toaster } from "../../components/ui/toaster.jsx";
 import { Map, MapMarker, ZoomControl } from "react-kakao-maps-sdk";
 import { categories } from "../../components/category/CategoryContainer.jsx";
 import { AuthenticationContext } from "../../components/context/AuthenticationProvider.jsx";
-import { ProductLike } from "../../components/product/ProductLike.jsx";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
+import { Avatar } from "../../components/ui/avatar.jsx";
+import { Rating } from "../../components/ui/rating.jsx";
+import { ProductLike } from "../../components/product/ProductLike.jsx";
+import { SlArrowRight } from "react-icons/sl";
+import { getDaysAgo } from "../../components/product/ProductDate.jsx";
 
 export function ProductView() {
   //  채팅방 만들때,   토큰에서 id 가져와야 하는데 , id   겹쳐서 > productId로  , >router도 변경함
@@ -32,38 +44,60 @@ export function ProductView() {
   const [likeData, setLikeData] = useState({});
   const [userLikes, setUserLikes] = useState(new Set());
   const [loading, setLoading] = useState(false);
+  const [profileImageUrl, setProfileImageUrl] = useState("");
+  const [rating, setRating] = useState(0.0);
   const navigate = useNavigate();
 
-  const { hasAccess, isAuthenticated, isAdmin, id } = useContext(
-    AuthenticationContext,
-  );
+  const { hasAccess, isAdmin, id } = useContext(AuthenticationContext);
 
+  // 상품 정보, 판매자 프로필 이미지, 판매자 평점 병렬 요청
   useEffect(() => {
-    // id >productId
-    axios
-      .get(`/api/product/view/${productId}`)
-      .then((res) => setProduct(res.data));
-  }, []);
+    const fetchProduct = async () => {
+      setLoading(true); // 로딩 시작
+      try {
+        // 상품 정보 요청
+        const productRes = await axios.get(`/api/product/view/${productId}`);
+        const productData = productRes.data;
+        setProduct(productData);
 
-  useEffect(() => {
-    if (product && product.latitude && product.longitude) {
-      setMarkerPosition({
-        lat: product.latitude,
-        lng: product.longitude,
-      });
-    }
-  }, [product]);
+        // 상품 데이터를 기반으로 추가 데이터 요청
+        const [profileImageRes, profileRatingRes] = await Promise.all([
+          axios.get("/api/myPage/image", {
+            params: { memberId: productData.writer },
+          }),
+          axios.get("/api/myPage/rating", {
+            params: { memberId: productData.writer },
+          }),
+        ]);
+
+        setProfileImageUrl(profileImageRes.data);
+        setRating(profileRatingRes.data);
+
+        // 위치 마커 설정
+        if (productData.latitude && productData.longitude) {
+          setMarkerPosition({
+            lat: productData.latitude,
+            lng: productData.longitude,
+          });
+        }
+      } catch (error) {
+        console.error("데이터를 가져오는 데 실패했습니다:", error);
+      } finally {
+        setLoading(false); // 로딩 종료
+      }
+    };
+
+    fetchProduct();
+  }, [productId]);
+
   // 상품과 좋아요 정보 동시에 로드
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchLikeData = async () => {
       try {
-        const [productRes, likeRes, userLikeRes] = await Promise.all([
-          axios.get(`/api/product/view/${productId}`),
+        const [likeRes, userLikeRes] = await Promise.all([
           axios.get("/api/product/likes"),
           axios.get("/api/product/like/member"),
         ]);
-
-        setProduct(productRes.data);
 
         const likes = likeRes.data.reduce((acc, item) => {
           acc[item.product_id] = item.like_count;
@@ -71,20 +105,16 @@ export function ProductView() {
         }, {});
 
         setLikeData(likes);
-        setUserLikes(new Set(userLikeRes.data)); // 사용자 좋아요 정보
-
+        setUserLikes(new Set(userLikeRes.data));
         setLoading(false);
       } catch (error) {
-        console.error("상품 정보를 가져오는데 오류가 발생했습니다.:", error);
+        console.error("상품 정보를 가져오는데 오류가 발생했습니다.", error);
+        setLoading(false);
       }
     };
 
-    fetchData();
-  }, [id]);
-
-  if (product === null) {
-    return <Spinner />;
-  }
+    fetchLikeData();
+  }, [productId, id]);
 
   const handleDeleteClick = () => {
     axios
@@ -96,11 +126,9 @@ export function ProductView() {
           description: data.message.text,
         });
 
-        // 관리자가 상품을 삭제한 경우, 해당 회원 상세 페이지로 이동
         if (isAdmin) {
           navigate(`/admin/members/${product.writer}/detail`);
         } else {
-          // 일반 사용자일 경우 상품 목록 페이지로 이동
           navigate("/product/list");
         }
       })
@@ -113,25 +141,12 @@ export function ProductView() {
       });
   };
 
-  if (product === null) {
-    return <Spinner />;
-  }
-
-  const categoryLabel =
-    categories.find((category) => category.value === product.category)?.label ||
-    "전체"; // 기본값 설정
-
-  if (!product && !likeData && !userLikes) {
-    return <Spinner />;
-  }
-
   // 챗 방 만들기
   const createChatRoom = () => {
-    var testId;
-    var productName = product.productName;
-    var writer = product.writer;
-    var nickname = "";
-    var buyer = id;
+    const productName = product.productName;
+    const writer = product.writer;
+    const nickname = "";
+    const buyer = id;
     axios
       .post("/api/chat/create", {
         productName: productName,
@@ -147,8 +162,91 @@ export function ProductView() {
       });
   };
 
+  // 카카오 맵 길찾기 링크 생성 함수
+  const getKakaoLink = () => {
+    return `https://map.kakao.com/link/to/${encodeURIComponent(
+      product.locationName,
+    )},${product.latitude},${product.longitude}`;
+  };
+
+  // 로딩 상태일 때 스피너를 표시
+  if (loading && !product) {
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+      >
+        <Spinner />
+      </Box>
+    );
+  }
+
+  // product가 null일 경우 데이터를 가져오지 못한 상황 처리
+  if (!product) {
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+      >
+        <Text>상품 정보를 가져올 수 없습니다.</Text>
+      </Box>
+    );
+  }
+  const categoryLabel =
+    categories.find((category) => category.value === product.category)?.label ||
+    "전체";
+
+  const daysAgo = getDaysAgo(product.createdAt);
+
   return (
-    <Box border={"1px solid black"}>
+    <Box mb={10}>
+      {(hasAccess(product.writer) || isAdmin) && (
+        <HStack mb={5} justifyContent="flex-end">
+          {hasAccess(product.writer) && (
+            <Button
+              colorPalette={"cyan"}
+              onClick={() => navigate(`/product/edit/${product.productId}`)}
+            >
+              수정
+            </Button>
+          )}
+
+          {(hasAccess(product.writer) || isAdmin) && (
+            <DialogRoot>
+              <DialogTrigger asChild>
+                <Button colorPalette={"red"}>삭제</Button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>삭제 확인</DialogTitle>
+                </DialogHeader>
+                <DialogBody>
+                  {isAdmin && (
+                    <Box color="red.500" fontSize="sm" mb={2}>
+                      관리자 권한으로 삭제하시겠습니까?
+                    </Box>
+                  )}
+                  {!isAdmin && (
+                    <p>등록한 {product.productId}번 상품을 삭제하시겠습니까?</p>
+                  )}
+                </DialogBody>
+                <DialogFooter>
+                  <DialogActionTrigger>
+                    <Button variant={"outline"}>취소</Button>
+                  </DialogActionTrigger>
+                  <Button colorPalette={"red"} onClick={handleDeleteClick}>
+                    삭제
+                  </Button>
+                </DialogFooter>
+              </DialogContent>
+            </DialogRoot>
+          )}
+        </HStack>
+      )}
       <Swiper
         slidesPerView={1}
         spaceBetween={30}
@@ -163,52 +261,80 @@ export function ProductView() {
         {product.fileList.map((file) => (
           <SwiperSlide className="product-page-swiper-slide">
             <Box>
-              <img src={file.src} alt={file.name} />
+              <Image src={file.src} alt={file.name} />
             </Box>
           </SwiperSlide>
         ))}
       </Swiper>
-      <Stack gap={5} border={"1px solid black"}>
-        <Box border={"1px solid black"}>판매자: {product.nickname}</Box>
-        <Flex gap={3}>
-          <Box minWidth="100px" border={"1px solid black"}>
-            <Field label={"카테고리"} readOnly>
-              <Input value={categoryLabel} />
-            </Field>
+
+      <Stack>
+        <HStack>
+          <Avatar
+            my={3}
+            boxSize="80px"
+            borderRadius="full"
+            fit="cover"
+            src={profileImageUrl}
+          />
+          <Heading size="md">판매자: {product.nickname}</Heading>
+          <Spacer />
+          <Rating readOnly value={rating} allowHalf size="md" mr={5} />
+        </HStack>
+
+        <HStack m={2} justifyContent="space-between" w="full">
+          <Heading>{product.productName}</Heading>
+          <Spacer />
+          <Box mr={5}>
+            <ProductLike
+              productId={product.productId}
+              initialLike={userLikes.has(product.productId)}
+              initialCount={likeData[product.productId] || 0}
+              isHorizontal={true}
+            />
           </Box>
-          <Box flex={8} border={"1px solid black"}>
-            <Field label={"상품명"} readOnly>
-              <Input value={product.productName} />
-            </Field>
+        </HStack>
+
+        <HStack m={2}>
+          <Box w="fit-content">
+            <Badge size="md" colorScheme="red">
+              {categoryLabel}
+            </Badge>
           </Box>
-        </Flex>
-        <Field border={"1px solid black"} label={"거래방식"} readOnly>
-          <Flex gap={4}>
-            <Button borderRadius="10px">
-              {product.pay == "sell" ? "판매하기" : "나눔하기"}
-            </Button>
-          </Flex>
-        </Field>
-        {product.pay === "sell" && (
-          <Field label={"가격"} readOnly border={"1px solid black"}>
-            <InputGroup flex="1" startElement={<PiCurrencyKrwBold />}>
-              <Input value={product.price} />
-            </InputGroup>
-          </Field>
-        )}
-        <Field border={"1px solid black"} label={"상품 설명"} readOnly>
-          <Textarea h={200} value={product.description} />
-        </Field>
-        <Field border={"1px solid black"} label={"거래 희망 장소"} readOnly>
-          <Input value={product.locationName} readOnly />
-        </Field>
-        <ProductLike
-          productId={product.productId}
-          initialLike={userLikes.has(product.productId)}
-          initialCount={likeData[product.productId] || 0}
-          isHorizontal={true} // 수평으로 하트와 숫자 배치
-        />
-        <Box border={"1px solid black"}>
+          <Text size="xs">{daysAgo}</Text>
+        </HStack>
+
+        <HStack justifyContent="space-between">
+          <Heading
+            ml={2}
+            color={product.pay === "sell" ? "gray.600" : undefined}
+          >
+            {product.pay === "sell" ? `${product.price}원` : "나눔"}
+          </Heading>
+          {product.writer !== id && (
+            <Button onClick={createChatRoom}>채팅하기</Button>
+          )}
+        </HStack>
+
+        <Textarea readOnly autoresize value={product.description} />
+
+        <HStack mt={3} justifyContent="space-between">
+          <Heading size="md">거래 희망 장소</Heading>
+          <a
+            href={getKakaoLink()}
+            style={{
+              textDecoration: "none",
+            }}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <HStack>
+              <span>{product.locationName}</span>
+              <SlArrowRight />
+            </HStack>
+          </a>
+        </HStack>
+
+        <Box display="flex" justifyContent="center" alignItems="center">
           <Map
             className="map"
             center={
@@ -218,62 +344,12 @@ export function ProductView() {
               }
             }
             level={3}
-            style={{ width: "100%", height: "400px" }}
+            style={{ width: "90%", height: "300px" }}
           >
             {markerPosition && <MapMarker position={markerPosition} />}
             <ZoomControl />
           </Map>
         </Box>
-        <Button onClick={createChatRoom} disabled={id === product.writer}>
-          거래하기
-        </Button>
-        {(hasAccess(product.writer) || isAdmin) && (
-          <Box>
-            {hasAccess(product.writer) && (
-              <Button
-                colorPalette={"cyan"}
-                onClick={() => navigate(`/product/edit/${product.productId}`)}
-              >
-                수정
-              </Button>
-            )}
-
-            {(hasAccess(product.writer) || isAdmin) && (
-              <DialogRoot>
-                <DialogTrigger asChild>
-                  <Button colorPalette={"red"}>삭제</Button>
-                </DialogTrigger>
-                <DialogContent>
-                  <DialogHeader>
-                    <DialogTitle>삭제 확인</DialogTitle>
-                  </DialogHeader>
-                  <DialogBody>
-                    {/* 관리자일 때만 표시되는 메시지 */}
-                    {isAdmin && (
-                      <Box color="red.500" fontSize="sm" mb={2}>
-                        관리자 권한으로 삭제하시겠습니까?
-                      </Box>
-                    )}
-                    {/* 관리자일 때는 일반 메시지를 표시하지 않도록 조건 추가 */}
-                    {!isAdmin && (
-                      <p>
-                        등록한 {product.productId}번 상품을 삭제하시겠습니까?
-                      </p>
-                    )}
-                  </DialogBody>
-                  <DialogFooter>
-                    <DialogActionTrigger>
-                      <Button variant={"outline"}>취소</Button>
-                    </DialogActionTrigger>
-                    <Button colorPalette={"red"} onClick={handleDeleteClick}>
-                      삭제
-                    </Button>
-                  </DialogFooter>
-                </DialogContent>
-              </DialogRoot>
-            )}
-          </Box>
-        )}
       </Stack>
     </Box>
   );
