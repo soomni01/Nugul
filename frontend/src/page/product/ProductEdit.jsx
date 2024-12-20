34a8de1ff58eff4338d310c3895944e6924ce038
soomni01 <dsgh8933@naver.com>
2024-12-20 10:56:41 +0900

    design: 상품 수정 페이지 ui 디자인

--- frontend/src/page/product/ProductEdit.jsx
+++ frontend/src/page/product/ProductEdit.jsx
@@ -2,14 +2,16 @@ import {
   Box,
   Flex,
   Heading,
+  HStack,
   Input,
+  Separator,
+  Spacer,
   Spinner,
   Stack,
   Text,
   Textarea,
 } from "@chakra-ui/react";
 import { useNavigate, useParams } from "react-router-dom";
-import { Field } from "../../components/ui/field.jsx";
 import { Button } from "../../components/ui/button.jsx";
 import { InputGroup } from "../../components/ui/input-group.jsx";
 import { PiCurrencyKrwBold } from "react-icons/pi";
@@ -30,6 +32,7 @@ import { categories } from "../../components/category/CategoryContainer.jsx";
 import { toaster } from "../../components/ui/toaster.jsx";
 import { AuthenticationContext } from "../../components/context/AuthenticationProvider.jsx";
 import { FcAddImage } from "react-icons/fc";
+import { IoIosArrowForward } from "react-icons/io";
 
 export function ProductEdit() {
   const { id } = useParams();
@@ -59,7 +62,7 @@ export function ProductEdit() {
       setFilesUrl(fileUrls);
       setFiles(product.fileList);
 
-      if (product.fileList && files.length > 0) {
+      if (!mainImage && product.fileList.length > 0) {
         setMainImage(product.fileList[0]);
       }
     }
@@ -194,85 +197,97 @@ export function ProductEdit() {
   }
 
   return (
-    <Box>
-      <Heading mb={3}>{id}번 상품 수정</Heading>
-      <Stack gap={5}>
-        <Flex alignItems="center">
-          <Box
-            p="10"
-            borderWidth="1px"
-            borderColor="lightgray"
-            borderRadius="10px"
-            onClick={() => fileInputRef.current?.click()}
-            cursor="pointer"
-            textAlign="center"
-          >
-            <input
-              ref={fileInputRef} // input 참조
-              onChange={handleFileUpload}
-              type="file"
-              accept="image/*"
-              multiple
-              style={{ display: "none" }}
-            />
-            <FcAddImage size="30px" />
-          </Box>
-
-          <Box
-            ml={3}
-            display="flex"
-            gap="10px"
-            overflowX="auto"
-            whiteSpace="nowrap"
-            minWidth="100px"
-          >
-            {filesUrl.map((file, index) => (
+    <Box display="flex" p={5} width="100%">
+      <Stack gap={5} width="100%">
+        <Heading mb={3}>{id}번 상품 수정</Heading>
+        <HStack alignItems="flex-start" width="100%">
+          <Text fontSize="lg" fontWeight="bold" textAlign="left" minWidth="15%">
+            상품 이미지
+          </Text>
+          <Spacer />
+          <Box width="85%">
+            <Flex alignItems="center" gap={3} mb={4}>
               <Box
-                key={index}
-                width="100px"
-                height="100px"
-                border="1px solid lightgray"
+                p="16"
+                borderWidth="1px"
+                borderColor="lightgray"
                 borderRadius="10px"
-                overflow="hidden"
+                onClick={() => fileInputRef.current?.click()}
                 cursor="pointer"
-                display="inline-block"
-                flexShrink={0} // 이미지가 축소되지 않도록 설정s
-                onClick={() => {
-                  handleRemoveClick(index, files[index]);
-                }}
+                textAlign="center"
               >
-                <img
-                  src={file}
-                  alt={`파일 미리보기: ${index}`}
-                  style={{
-                    width: "100%",
-                    height: "100%",
-                    objectFit: "cover",
-                  }}
+                <input
+                  ref={fileInputRef} // input 참조
+                  onChange={handleFileUpload}
+                  type="file"
+                  accept="image/*"
+                  multiple
+                  style={{ display: "none" }}
                 />
+                <FcAddImage size="40px" />
               </Box>
-            ))}
-          </Box>
-        </Flex>
-        <Text size="xs" mt={-2}>
-          {!fileInputInvalid ? (
-            "가장 처음 이미지가 대표이미지입니다."
-          ) : (
-            <Text color="red">
-              각 파일은 1MB 이하, 총 용량은 10MB 이하이어야 합니다.
-            </Text>
-          )}
-        </Text>
 
-        <Flex gap={3}>
-          <Box minWidth="100px">
-            <Field label={"카테고리"}>
+              <Box display="flex" gap="10px" overflowX="auto">
+                {filesUrl.map((file, index) => (
+                  <Box
+                    key={index}
+                    width="150px"
+                    height="150px"
+                    border="1px solid lightgray"
+                    borderRadius="10px"
+                    overflow="hidden"
+                    cursor="pointer"
+                    display="inline-block"
+                    flexShrink={0} // 이미지가 축소되지 않도록 설정s
+                    onClick={() => {
+                      handleRemoveClick(index, files[index]);
+                    }}
+                  >
+                    <img
+                      src={file}
+                      alt={`파일 미리보기: ${index}`}
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                      }}
+                    />
+                  </Box>
+                ))}
+              </Box>
+            </Flex>
+
+            {fileInputInvalid && (
+              <Text fontSize="sm" color="red" mb={3}>
+                각 파일은 1MB 이하, 총 용량은 10MB 이하이어야 합니다.
+              </Text>
+            )}
+
+            <Text fontSize="sm" color="blue.400" whiteSpace="pre-line">
+              - 가장 처음 이미지가 대표이미지입니다.{"\n"}- 이미지는 상품 등록
+              시 정사각형으로 짤려서 등록됩니다.{"\n"}- 이미지를 클릭하여 삭제할
+              수 있습니다. {"\n"}- 큰 이미지일경우 이미지가 깨지는 경우가 발생할
+              수 있습니다. {"\n"}- 각 파일은 1MB 이하, 총 용량은 10MB 이하이어야
+              합니다.
+            </Text>
+          </Box>
+        </HStack>
+
+        <Separator />
+
+        <HStack alignItems="flex-start">
+          <Text fontSize="lg" fontWeight="bold" minWidth="15%">
+            상품명
+            <span style={{ color: "red" }}>*</span>
+          </Text>
+          <Flex alignItems="center" w="85%" gap={3}>
+            <Box minWidth="100px">
               <select
                 value={product.category}
                 onChange={handleCategoryChange}
                 style={{
                   width: "100px",
-                  padding: "8px",
+                  padding: "11px",
                   borderRadius: "4px",
                   border: "1px solid #ccc",
                 }}
@@ -283,72 +298,118 @@ export function ProductEdit() {
                   </option>
                 ))}
               </select>
-            </Field>
-          </Box>
-          <Box flex={8}>
-            <Field label={"상품명"}>
+            </Box>
+            <Box width="100%">
               <Input
+                size="xl"
                 value={product.productName}
+                placeholder="상품명을 입력하세요"
                 onChange={(e) =>
                   setProduct({ ...product, productName: e.target.value })
                 }
               />
-            </Field>
-          </Box>
-        </Flex>
-        <Field label={"거래방식"}>
-          <Flex gap={4}>
-            <Button
-              borderRadius="10px"
-              variant={product.pay === "sell" ? "solid" : "outline"} // 판매하기 버튼 스타일
-              onClick={() => handleButtonClick("sell")} // 판매하기 버튼 클릭 시
-            >
-              판매하기
-            </Button>
-            <Button
-              borderRadius="10px"
-              variant={product.pay === "share" ? "solid" : "outline"} // 나눔하기 버튼 스타일
-              onClick={() => handleButtonClick("share")} // 나눔하기 버튼 클릭 시
-            >
-              나눔하기
-            </Button>
+            </Box>
           </Flex>
-        </Field>
-        {product.pay === "sell" && (
-          <InputGroup w={"20%"} flex="1" startElement={<PiCurrencyKrwBold />}>
-            <Input value={product.price} onChange={handlePriceChange} />
-          </InputGroup>
-        )}
-        <Field label={"상품 설명"}>
-          <Textarea
-            placeholder="등록할 상품의 게시글 내용을 작성해주세요."
-            h={150}
-            value={product.description}
-            onChange={(e) =>
-              setProduct({ ...product, description: e.target.value })
-            }
-          />
-        </Field>
-        <Field label={"거래 희망 장소"}>
-          <Input
-            value={product.locationName}
-            onClick={() => setIsModalOpen(true)} // 거래 장소 input 클릭 시 모달 열기
-            readOnly
-          />
-        </Field>
+        </HStack>
+
+        <Separator />
+
+        <HStack alignItems="flex-start">
+          <Text fontSize="lg" fontWeight="bold" minWidth="15%">
+            거래 방식
+            <span style={{ color: "red" }}>*</span>
+          </Text>
+          <Flex alignItems="flex-start" w="85%" gap={4} direction="column">
+            <HStack>
+              <Button
+                size="lg"
+                borderRadius="10px"
+                variant={product.pay === "sell" ? "solid" : "outline"}
+                onClick={() => handleButtonClick("sell")}
+              >
+                판매하기
+              </Button>
+              <Button
+                borderRadius="10px"
+                variant={product.pay === "share" ? "solid" : "outline"}
+                onClick={() => handleButtonClick("share")}
+              >
+                나눔하기
+              </Button>
+            </HStack>
+
+            {product.pay === "sell" && (
+              <InputGroup
+                w={"20%"}
+                flex="1"
+                startElement={<PiCurrencyKrwBold />}
+              >
+                <Input
+                  value={product.price}
+                  onChange={handlePriceChange}
+                  placeholder="가격을 입력하세요"
+                />
+              </InputGroup>
+            )}
+          </Flex>
+        </HStack>
+
+        <Separator />
+
+        <HStack alignItems="flex-start">
+          <Text fontSize="lg" fontWeight="bold" minWidth="15%">
+            상품 설명
+            <span style={{ color: "red" }}>*</span>
+          </Text>
+          <Flex alignItems="center" w="85%">
+            <Textarea
+              placeholder="등록할 상품의 게시글 내용을 작성해주세요."
+              h={150}
+              value={product.description}
+              onChange={(e) =>
+                setProduct({ ...product, description: e.target.value })
+              }
+            />
+          </Flex>
+        </HStack>
+
+        <Separator />
+
+        <HStack alignItems="flex-start">
+          <Text fontSize="lg" fontWeight="bold" minWidth="15%">
+            거래 장소
+            <span style={{ color: "red" }}>*</span>
+          </Text>
+          <Flex alignItems="center" minWidth="85%">
+            <InputGroup flex="1" endElement={<IoIosArrowForward />}>
+              <Input
+                value={product.locationName}
+                onClick={() => setIsModalOpen(true)}
+                readOnly
+              />
+            </InputGroup>
+          </Flex>
+        </HStack>
+
         <MapModal
           isOpen={isModalOpen}
           onClose={() => setIsModalOpen(false)}
           onSelectLocation={handleLocationSelect}
         />
+
         {hasAccess(product.writer) && (
-          <Box>
+          <Box display="flex" w="100%" justifyContent="flex-end">
             <DialogRoot
               open={dialogOpen}
               onOpenChange={(e) => setDialogOpen(e.open)}
             >
               <DialogTrigger asChild>
-                <Button disabled={disabled} colorPalette={"blue"}>
+                <Button
+                  w="10%"
+                  size="lg"
+                  disabled={disabled}
+                  colorPalette={"blue"}
+                >
                   저장
                 </Button>
               </DialogTrigger>
@@ -363,6 +424,7 @@ export function ProductEdit() {
                   <DialogActionTrigger>
                     <Button variant={"outline"}>취소</Button>
                   </DialogActionTrigger>
+
                   <Button
                     loading={progress}
                     colorPalette={"blue"}
