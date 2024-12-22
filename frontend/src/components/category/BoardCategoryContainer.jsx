91cd1bbf868dd36fdeb14f75a149d84518b3cb82
keaimk <fly1043@naver.com>
2024-12-23 06:06:18 +0900

    design : 게시판 카테고리 디자인 수정

--- frontend/src/components/category/BoardCategoryContainer.jsx
+++ frontend/src/components/category/BoardCategoryContainer.jsx
@@ -1,31 +1,36 @@
-import {Box, Button, Flex, HStack} from "@chakra-ui/react";
+import { Button, HStack } from "@chakra-ui/react";
 
 export function BoardCategoryContainer({ selectedCategory, onCategorySelect }) {
-    return (
-        <HStack my={5} spacing={0} w="100%" justify="space-between">
-            {BoardCategories.map((category) => (
-                <Box
-                    key={category.value}
-                    flex="1"
-                    textAlign="center"
-                    onClick={() => onCategorySelect(category.value)}
-                    bg={selectedCategory === category.value ? "gray.200" : "transparent"}
-                    _hover={{ cursor: "pointer", bg: "gray.100" }}
-                    p={2} // 클릭 영역을 넓히기 위한 패딩
-                >
-                    {category.label}
-                </Box>
-            ))}
-        </HStack>
-    );
+  return (
+    <HStack my={5} w="70%" justify="center" mx="auto">
+      {BoardCategories.map((category) => (
+        <Button
+          key={category.value}
+          flex="1"
+          variant="outline"
+          colorScheme="blue"
+          onClick={() => onCategorySelect(category.value)}
+          bg={selectedCategory === category.value ? "blue.100" : "transparent"}
+          _hover={{ bg: "blue.50" }}
+          _active={{ bg: "blue.200" }}
+          _focus={{ boxShadow: "none" }}
+          borderRadius="full"
+          fontSize="lg"
+          h="50px"
+        >
+          {category.label}
+        </Button>
+      ))}
+    </HStack>
+  );
 }
 
 // boardCategories 배열을 정의하고 export
 export const BoardCategories = [
-    {label: "전체", value: "all"},
-    {label: "맛집", value: "restaurant"},
-    {label: "생활", value: "life"},
-    {label: "리뷰", value: "review"},
-    {label: "고민", value: "concern"},
-    {label: "기타", value: "other"},
+  { label: "전체", value: "all" },
+  { label: "맛집", value: "restaurant" },
+  { label: "생활", value: "life" },
+  { label: "리뷰", value: "review" },
+  { label: "고민", value: "concern" },
+  { label: "기타", value: "other" },
 ];
